24741f6aba1b9331d7420cd597147172ae1077e5
Michael Allen <michaeldfallen@gmail.com>
2013-08-16 12:41:39 +0100

    Update update-template.sh
    adding cd to update_template.sh directory. Ensures script works even if executed from outside the directory.

--- update-template.sh
+++ update-template.sh
@@ -1,6 +1,6 @@
 thisDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-templateDir="$thisDir/govuk_template_play"
 
+cd "$thisDir"
 echo "Updating govuk_template_play"
 git submodule init
 git submodule update
